94d960e034f501e8d4e56915efa47d8973ca674f
David Rogers <al.the.x@gmail.com>
2015-06-11 12:41:11 -0400

    Simple replacement with jQuery...

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
+;(function(window){
+  var $name = $('.profile-name big'),
+      $username = $('.profile-name small');
+
+  $.getJSON('/api/github/users/al-the-x.json')
+    /**
+     * Replacing placeholders with data from...?
+     *
+     * @param user
+     * @return ???
+     */
+    .then(function(user){
 
 
-
+      $name.text(user.name);
+      $username.text(user.login);
+    })
 })(window);
